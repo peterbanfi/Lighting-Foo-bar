ef16ff5fb29860319113639f099e8e620fd9f0f1
Banfi Peter <p6t6rbanfi@gmail.com>
2018-05-14 14:49:09 +0200

    Feat: updated reg

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Http, RequestOptions } from '@angular/http';
+
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  user: any = {
+    username: '',
+    email: '',
+    password: '',
+    passwordConf: '',
+    rights: true
+  };
 
-  constructor() { }
+  baseUrl = 'http://localhost:8080/user/register';
+  constructor(public http: Http) { }
 
   ngOnInit() {
+  }
+
+  register() {
+    console.log(this.user);
+    this.http.post(this.baseUrl, this.user)
+      .subscribe(data => {
+        console.log(data['_body']);
+      });
+  }
+
+  selectChangeHandler(event: any) {
+    this.user.rights = event.target.value;
   }
 
 }
