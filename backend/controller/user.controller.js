7fe39763f4e8e75f28f3a34b56cc870dbb892627
Banfi Peter <p6t6rbanfi@gmail.com>
2018-05-14 14:20:07 +0200

    Feat: Login and Registration

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -9,9 +9,9 @@ module.exports = {
 
   register: (req, res) => {
     User.register(new User({
-      username: req.body.username,
-      email: req.body.email,
-    }), req.body.password)
+        username: req.body.username,
+        email: req.body.email,
+      }), req.body.password)
       .then(() => res.json({
         success: 'Sikeres regisztráció',
       }))
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,36 +7,5 @@ import { Http, RequestOptions } from '@angular/http';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    user: any = {
-        username: 'YOUR RESGISTERED USERNAME',
-        password: 'YOUR RESGISTERED USER PASSWORD'
-    };
 
-    options = new RequestOptions({ withCredentials: true });
-    baseUrl = 'http://localhost:8080/user/';
-
-    constructor(public http: Http) {
-
-    }
-
-    profile() {
-        this.http.get(this.baseUrl + 'profile', this.options)
-            .subscribe(data => {
-                console.log(data['_body']);
-            });
-    }
-
-    login() {
-        this.http.post(this.baseUrl + 'login', this.user, this.options)
-            .subscribe(data => {
-                console.log(data['_body']);
-            });
-    }
-
-    logout() {
-        this.http.get(this.baseUrl + 'logout', this.options)
-            .subscribe(data => {
-                console.log(data['_body']);
-            });
-    }
 }
