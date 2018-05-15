67ab51b1e77fa8259264c72127001d3eaed1ad0d
Linda Dubeczi <dublimi5@yahoo.com>
2018-05-15 09:06:42 +0200

    fix: user modal

--- backend/models/user.js
+++ backend/models/user.js
@@ -24,6 +24,7 @@ const userSchema = mongoose.Schema({
 /**
  * Beállítva az 5 elrontott próbálkozás után 3 percre kitiltás
  * email címmel lehet belépni
+ * a jelszónak min 8 karakternek kell lennie
  */
 userSchema.plugin(passportLocalMongoose, {
   usernameField: 'email',
@@ -32,7 +33,7 @@ userSchema.plugin(passportLocalMongoose, {
   hashField: 'password',
   passwordValidator: (password, cb) => {
     if (!validator.isLength(password, 8)) {
-      return cb({code: 400, message: 'A megadott jelszónak legalább 8 karakter hosszúnak kell lennie.'});
+      return cb({code: 400, message: 'A megadott jelszónak legalább 8 karakter hosszúnak kell lennie.' });
     }
     return cb(null);
   },
