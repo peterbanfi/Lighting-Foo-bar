ca51cc4821567e56afb73b72c69421e2005bc648
Targaryen57 <marton.rubi@gmail.com>
2018-05-16 23:50:05 +0200

    deleted unused modules

--- frontend/src/app/http-products.service.ts
+++ frontend/src/app/http-products.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
-
 import { HttpClient } from '@angular/common/http';
-import { Http, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HttpProductsService {
-  options = new RequestOptions({ withCredentials: true });
 
   constructor(public http: HttpClient) { }
 
