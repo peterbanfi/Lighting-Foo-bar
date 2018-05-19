89f27d050ffa71ac29015cde592fba8ca70ce89d
Banfi Peter <p6t6rbanfi@gmail.com>
2018-05-19 17:40:52 +0200

    Feat: home page first steps

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpProductsService } from '../http-products.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  baseUrl = 'http://localhost:8080/products';
+  products: any;
+  lastTenProducts: any = [];
+  constructor(private http: HttpProductsService) {
+    this.list();
 
-  constructor() { }
-
-  ngOnInit() {
   }
 
+  ngOnInit() { }
+
+  list() {
+    this.http.getAll(this.baseUrl)
+      .then((res) => {
+        this.products = res;
+        console.log(this.products);
+      })
+      .then(() => {
+        this.lastTen(this.products);
+      });
+
+  }
+
+  lastTen(data) {
+    console.log(data);
+    for (let i = data.length; i > 0; i--) {
+      this.lastTenProducts.push(data[i]);
+    }
+    console.log(this.lastTenProducts);
+    /*   for (let i = 0; i < 11; i++) {
+        const element = array[i];
+      } */
+  }
 }
